45d23ee7c259e07784d55e28493468d3d65d0c8f
LiamOgu <augu.liam@gmail.com>
2025-12-21 18:43:45 +0100

    feat : create, read for users collection

---
+++ server/src/controllers/usersController.ts
@@ -0,0 +1,94 @@
+import type { Request, Response } from "express";
+import { ObjectId } from "mongodb";
+import {
+  createUser,
+  getAllUsers,
+  getUserByEmail,
+  getUserById,
+} from "../services/usersService.ts";
+import type { UserInput } from "../types/UserInterface.ts";
+
+export const createUserController = async (req: Request, res: Response) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        error: "Missing required fields",
+      });
+    }
+
+    const userData: UserInput = {
+      username,
+      email,
+      passwordHash: password,
+    };
+
+    const newUserId = await createUser(userData);
+
+    return res.status(201).json({ userId: newUserId });
+  } catch (error) {
+    console.error("Error in createUserController:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getAllUsersController = async (_: any, res: Response) => {
+  try {
+    const users = await getAllUsers();
+    if (users && users.length > 0) {
+      return res.status(200).json({ users });
+    }
+    return res.status(200).json({ users: [] });
+  } catch (error) {
+    console.error("Error in getAllUsersController:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getUserByEmailController = async (req: Request, res: Response) => {
+  try {
+    const email = req.params.email;
+
+    if (!email) {
+      return res.status(400).json({ error: "Email parameter is required" });
+    }
+
+    const user = await getUserByEmail(email);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.error("Error in getUserByEmailController:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getUserByIdController = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    if (!id) {
+      return res.status(400).json({ error: "ID parameter is required" });
+    }
+
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).json({ error: "Invalid user ID format" });
+    }
+
+    const objectId = new ObjectId(id);
+    const user = await getUserById(objectId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.error("Error in getUserByIdController:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
