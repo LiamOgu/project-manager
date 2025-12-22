51b6dd520376b09fc5fef8e49678f5679b7a8340
LiamOgu <augu.liam@gmail.com>
2025-12-22 18:54:16 +0100

    add unique index on projects name

--- server/src/utils/indexesUtils.ts
+++ server/src/utils/indexesUtils.ts
@@ -1,7 +1,9 @@
 import { getDb } from "../db/mongo.ts";
+import type { Project } from "../types/ProjectInterface.ts";
 import type { User } from "../types/UserInterface.ts";
 
 const getUsersCollection = () => getDb().collection<User>("users");
+const getProjectsCollection = () => getDb().collection<Project>("projects");
 
 export const createEmailIndex = async (): Promise<void> => {
   try {
@@ -9,6 +11,16 @@ export const createEmailIndex = async (): Promise<void> => {
     console.log("Unique index created on email field");
   } catch (error) {
     console.error("Error creating email index:", error);
+    throw error;
+  }
+};
+
+export const createProjectNameIndex = async (): Promise<void> => {
+  try {
+    await getProjectsCollection().createIndex({ name: 1 }, { unique: true });
+    console.log("Unique index created on project name field");
+  } catch (error) {
+    console.error("Error creating project name index:", error);
     throw error;
   }
 };
