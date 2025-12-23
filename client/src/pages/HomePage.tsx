96277a40e5c6aa6d7bc7edaea7adadf1641b5462
LiamOgu <augu.liam@gmail.com>
2025-12-23 20:18:23 +0100

    style : little style change on error fetching projects

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -34,8 +34,10 @@ export default function HomePage() {
               <span className="loading loading-spinner loading-lg"></span>
             </div>
           ) : isError ? (
-            <div className="alert alert-error">
-              <span>Erreur: {String(error)}</span>
+            <div className="alert">
+              <span>
+                Impossible de trouver les projets. Erreur : {String(error)}
+              </span>
             </div>
           ) : (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
